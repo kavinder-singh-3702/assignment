2c9231e3af9477accfc422f8adf8448ab786437c
Kavinder Singh <kavinder.singh.3702@gmail.com>
2024-03-05 07:27:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./sections/nav";
+import About from "./sections/about_bitcoin";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <About />
+    </>
+  );
+}
+
+export default App;
